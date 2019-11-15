1faedddd478a3c9e6d3983df8deff9ccb494d8be
wu-jeffrey <jeff.jiang.wu@gmail.com>
2019-11-15 17:27:07 -0500

    start frontend, moving to desktop

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import './Home.css';
+import './home.css';
+import Search from '../search/search.js'
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 function Home() {
   return (
     <div className="Home">
-      
+      <Card>
+        <CardContent>
+          Testtest
+        </CardContent>
+      </Card>
+
+      <Search></Search>
     </div>
   );
 }
--- client/src/components/search/search.js
+++ client/src/components/search/search.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './search.css';
 
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+
 function Search() {
   return (
     <div className="Search">
-
+      <Paper component="form">
+        <InputBase
+          placeholder="Search Food"
+          inputProps={{ 'aria-label': 'search food' }}
+        />
+        <IconButton type="submit" aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
     </div>
   );
 }

77a9c436c6216f202d385ebd1c33986f8954bc49
wu-jeffrey <jeff.jiang.wu@gmail.com>
2019-11-15 11:04:33 -0500

    nutritionix query api for autofill searchbar working

---
+++ client/src/components/fridge/fridge.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './fridge.css';
+
+function Fridge() {
+  return (
+    <div className="Fridge">
+
+    </div>
+  );
+}
+
+export default Fridge;
--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const https = require('https');
 const path = require('path');
 const app = express();
 
 // API routes
-app.get('/api/foods', (req, res) => {
+// TODO: Move to separate file
+app.get('/api/nutritionix/:query', (req, res) => {
+  // TODO: use regex to escape chars in query
+  let query = req.params.query || '';
+  var options = {
+    hostname: 'trackapi.nutritionix.com',
+    path: `/v2/search/instant?query=${query}`,
+    method : 'GET',
+    headers: {
+      "x-app-id": "ea96c6a3",
+      "x-app-key": "6349b402aa732468a3cf1c258f7a6708"
+    }
+  }
 
+  var request = https.request(options, function(response){
+    var body = ""
+    response.on('data', function(data) {
+      body += data;
+    });
+    response.on('end', function() {
+      res.send(JSON.parse(body));
+    });
+  });
+  request.on('error', function(e) {
+    console.log('Request Error: ' + e.message);
+  });
+  request.end();
 });
 
 const PORT = process.env.port || 5000;

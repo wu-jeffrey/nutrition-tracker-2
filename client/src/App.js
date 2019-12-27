59e8d6ab81a089782d254253a778e17e144ee036
wu-jeffrey <jeff.jiang.wu@gmail.com>
2019-12-27 15:52:26 -0500

    [ADD USER] Register/signup UI without logic

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Home from './components/home/home.js';
 import MenuBar from './components/menuBar/menuBar.js';
+import SignUp from './components/signUp/signUp.js'
 
 function App() {
   return (
     <div className="app">
+      {/* <SignUp></SignUp> */}
       <MenuBar></MenuBar>
       <Home></Home>
     </div>

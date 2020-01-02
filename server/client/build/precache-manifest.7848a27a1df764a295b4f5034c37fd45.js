07f04e134ef94d9fa039e88bd8accd264af26596
wu-jeffrey <jeff.jiang.wu@gmail.com>
2020-01-02 00:23:52 -0500

    [Deploy] Ugh I give up, manually drag build from client to server file

---
+++ server/client/build/precache-manifest.7848a27a1df764a295b4f5034c37fd45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd10bb0a2803374234be8da7b729c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d82a9d594b13885df0",
+    "url": "/static/css/main.fd657555.chunk.css"
+  },
+  {
+    "revision": "1e5962868a9da679818f",
+    "url": "/static/js/2.547efd5f.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/static/js/2.547efd5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7d82a9d594b13885df0",
+    "url": "/static/js/main.6cb5f1ad.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);

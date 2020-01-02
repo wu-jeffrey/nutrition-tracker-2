4c8802ecbe12b6dd7aaa888ca0584b6af55b74e5
wu-jeffrey <jeff.jiang.wu@gmail.com>
2020-01-02 00:27:24 -0500

    >:(

--- server/app.js
+++ server/app.js
@@ -14,11 +14,11 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 // if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 // }
 

b576d38dc8046548e65f116d51301f65eeb6f47e
wu-jeffrey <jeff.jiang.wu@gmail.com>
2022-06-08 18:35:50 -0400

    feat: add settings and progress scaffold

---
+++ client/src/sections/Progress/Progress.js
@@ -0,0 +1,8 @@
+export function Progress() {
+  return (
+    <>
+      <h1>Progress</h1>
+      <p>Coming Soon!</p>
+    </>
+  )
+}
---
+++ client/src/sections/Settings/Settings.js
@@ -0,0 +1,8 @@
+export function Settings() {
+  return (
+    <>
+      <h1>Settings</h1>
+      <p>Coming Soon!</p>
+    </>
+  )
+}
